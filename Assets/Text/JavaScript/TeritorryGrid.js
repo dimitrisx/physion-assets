f5abfd9b98c4da2c89d3cd93a8977511adc4c551
MysteriousDeve <88138142+MysteriousDeve@users.noreply.github.com>
2022-01-31 15:18:27 +0700

    Update TeritorryGrid.js

--- Assets/Text/JavaScript/TeritorryGrid.js
+++ Assets/Text/JavaScript/TeritorryGrid.js
@@ -2,39 +2,30 @@
   Grid generator for Teritorry Wars or Multiply || Release or stuffs.
   Written by Box.
 */
-
-class TeritorryGrid
-{
-  constructor (width, height, boxSize, x, y)
-  {
-    this.w = width || 8;
-    this.h = height || 8;
-    this.b = boxSize || 1;
-    this.x = x;
-    this.y = y;
-  }
-  
-  async create (scene, pos)
-  {
-    for (let i = 0; i < w; i++) {
-	  	for (let j = 0; j < h; j++) {
-	  		createBox(i, j);
-	  	}
-	  }
-  }
-  
-  createBox (i, j)
-  {
-    const posx = (i - w / 2) * b + b / 2 + x;
-		const posy = (j - h / 2) * b + b / 2 + y;
-		const f = 1.001;
-		const box = new physion.RectangleNode(b * f, b * f);
-		box.initNode(posx, posy);
-		box.bodyType = "Static";
-		box.fillColor = physion.pixiUtils.string2hex("#606060");
-		box.friction = 0;
-		box.restitution = 1;
-		box.lineAlpha = 0;
-		physion.root.scene.addChild(box);
-  }
+class TeritorryGrid {
+    constructor(width, height, boxSize, x, y, drawBorder, overlapping) {
+        this.width = width || 8;
+        this.height = height || 8;
+        this.boxSize = boxSize || 1;
+        this.x = x || 0;
+        this.y = y || 0;
+        this.drawBorder = drawBorder || true;
+        this.overlapping = overlapping || true
+    }
+    async create() {
+        for (let i = 0; i < this.width; i++) {
+            for (let j = 0; j < this.height; j++) {
+                const posx = (i - this.width / 2) * this.boxSize + this.boxSize / 2 + this.x;
+                const posy = (j - this.height / 2) * this.boxSize + this.boxSize / 2 + this.y;
+                const f = 1 + (this.overlapping ? this.boxSize / 1000.0 : 0);
+                const box = new physion.RectangleNode(this.boxSize * f, this.boxSize * f);
+                box.initNode(posx, posy);
+                box.bodyType = "Static";
+                box.friction = 0;
+                box.restitution = 1;
+                box.lineAlpha = (this.drawBorder ? 1 : 0);
+                physion.root.scene.addChild(box);
+            }
+        }
+    }
 }
