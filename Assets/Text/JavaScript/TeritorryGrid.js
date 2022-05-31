96c31721759d246d801b1ccf62a3c7e5ae05ad0e
MysteriousDeve <88138142+MysteriousDeve@users.noreply.github.com>
2022-05-31 20:00:41 +0700

    Remove unnessary option in TerritoryGrid.js

--- Assets/Text/JavaScript/TeritorryGrid.js
+++ Assets/Text/JavaScript/TeritorryGrid.js
@@ -10,15 +10,13 @@ class TeritorryGrid {
 		this.x = x || 0;
 		this.y = y || 0;
 		this.drawBorder = drawBorder || true;
-		this.overlapping = overlapping || true
 	}
 	async create(scene, position) {
 		for (let i = 0; i < this.width; i++) {
 			for (let j = 0; j < this.height; j++) {
 				const posx = (i - this.width / 2) * this.boxSize + this.boxSize / 2 + this.x;
 				const posy = (j - this.height / 2) * this.boxSize + this.boxSize / 2 + this.y;
-				const f = 1 + (this.overlapping ? this.boxSize / 1000.0 : 0);
-				const box = new physion.RectangleNode(this.boxSize * f, this.boxSize * f);
+				const box = new physion.RectangleNode(this.boxSize, this.boxSize);
 				box.initNode(posx, posy);
 				box.bodyType = "static";
 				box.friction = 0;
