2da18f8f146632321de355546ad6befbbcf8b5d0
MysteriousDeve <88138142+MysteriousDeve@users.noreply.github.com>
2022-05-31 20:43:19 +0700

    Update TerritoryHex.js

--- Assets/Text/JavaScript/TerritoryHex.js
+++ Assets/Text/JavaScript/TerritoryHex.js
@@ -5,40 +5,39 @@
 class TeritorryGrid
 {
 	constructor(width, height, hexSize, x, y, drawBorder) //, isFlatTop) 
-    {
+    	{
 		this.width = width || 8;
 		this.height = height || 8;
 		this.hexSize = hexSize || 1;
 		this.x = x || 0;
 		this.y = y || 0;
 		this.drawBorder = drawBorder || true;
-        // this.isFlatTop = isFlatTop || true;
+        	// this.isFlatTop = isFlatTop || true;
 	}
 
 	async create(scene, position) 
-    {
-        const a = Math.sqrt(3) / 2.0;
+	{
+        	const a = Math.sqrt(3) / 2.0;
 
 		for (let i = 0; i < this.width; i++) 
-        {
+      	  	{
 			for (let j = 0; j < this.height; j++) 
-            {
-				const posx = i * .75 * this.hexSize + this.x;
-				const posy = (j + (i % 2 == 1 ? .5 : 0)) * this.hexSize + this.y;
+            		{
+				const posx = i * 1.5 * this.hexSize + this.x;
+				const posy = (j * a * 2 + (i % 2 == 1 ? a : 0)) * this.hexSize + this.y;
 
 				const hex = new physion.PolygonNode();
-                hex.initNode(posx, posy);
+                		hex.initNode(posx, posy);
 
-                var array = [[]];
-                for (var z = 0; z < 6; z++)
-                {
-                    array[0].push({
-                        x: Math.cos(z * Math.PI / 3.0),
-                        y: Math.sin(z * Math.PI / 3.0)
-                    })
-                }
-                hex.polygons = array;
-				
+                		var array = [[]];
+                		for (var z = 0; z < 6; z++)
+                		{
+                		    	array[0].push({
+                		        	x: Math.cos(z * Math.PI / 3.0),
+                 		       		y: Math.sin(z * Math.PI / 3.0)
+                    			})
+                		}
+                		hex.polygons = array;
 				hex.bodyType = "static";
 				hex.friction = 0;
 				hex.restitution = 1;
