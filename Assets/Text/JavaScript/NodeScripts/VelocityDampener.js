a35b4ff7d771af1d01bde786fff8d7a8c5a43dfa
Aiden <aidenatmoko@gmail.com>
2024-09-24 21:31:05 +0700

    Updated VelocityDampener according to feedback

--- Assets/Text/JavaScript/NodeScripts/VelocityDampener.js
+++ Assets/Text/JavaScript/NodeScripts/VelocityDampener.js
@@ -5,18 +5,24 @@
  */
 class VelocityDampener {
 
+    // Script initializer
     constructor(node) {
         this.node = node;
         this.linearForce = 5; // Linear velocity dampening force
         this.angularForce = 5; // Angular velocity dampening force
-        this.scene = this.node.findSceneNode();
+        this.scene = undefined;
     }
 
+    // Update loop
     update(delta) {
-        // Get dampening forces and dampen velocity components
-        const fv = Math.min(1, this.linearForce * this.scene.timeStep);
-        this.node.linearVelocityX -= this.node.linearVelocityX * fv;
-        this.node.linearVelocityY -= this.node.linearVelocityY * fv;
-        this.node.angularVelocity -= this.node.angularVelocity * Math.min(1, this.angularForce * this.scene.timeStep);
+        if (this.scene === undefined) this.scene = this.node.findSceneNode(); // Check if we haven't found the scene yet
+        if (this.scene) {
+            // Get dampening forces and dampen velocity components
+            const fv = Math.min(1, this.linearForce * this.scene.timeStep),
+                fva = Math.min(1, this.angularForce * this.scene.timeStep);
+            this.node.linearVelocityX -= this.node.linearVelocityX * fv;
+            this.node.linearVelocityY -= this.node.linearVelocityY * fv;
+            this.node.angularVelocity -= this.node.angularVelocity * fva;
+        }
     }
 }
